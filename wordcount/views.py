ded40c7480cc030137b4787a2ed8fe91b072d930
sujpac <sujit.pack@gmail.com>
2021-02-01 01:26:07 -0800

    first commit

---
+++ wordcount/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    fulltext = request.GET['fulltext']
+
+    wordlist = fulltext.split()
+
+    worddict = {}
+
+    for word in wordlist:
+        if word in worddict:
+            worddict[word] += 1
+        else:
+            worddict[word] = 1
+
+    sortedwords = sorted(worddict.items(), key=lambda x: x[1], reverse=True)
+
+    return render(request, 'count.html', {'fulltext': fulltext,
+                                          'count': len(wordlist),
+                                          'sortedwords': sortedwords,
+                                          })
+
+def about(request):
+    return render(request, 'about.html')
